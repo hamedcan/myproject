d895f5b54f46c6382a77436b50202418917cb29e
hamedcan <32765912+hamedcan@users.noreply.github.com>
2017-11-09 23:21:13 +0330

    deconvolution and conv added

--- Model_3Dunet.py
+++ Model_3Dunet.py
@@ -1,9 +1,10 @@
 import numpy as np
 from keras import backend as K
 from keras.engine import Input, Model
-from keras.layers import Conv3D, MaxPooling3D, UpSampling3D, Activation, Dropout, BatchNormalization, Deconv3D
+from keras.layers import Conv3D, MaxPooling3D, UpSampling3D, Activation, Dropout, BatchNormalization
 from keras import optimizers
 from keras.layers.merge import concatenate
+# from keras_contrib.layers import Deconvolution3D
 
 def get_model(logger, log_disable ,input_shape, pool_size=(2, 2, 2), filter_size=(3, 3, 3), n_labels=1):
     """
@@ -28,21 +29,18 @@ def get_model(logger, log_disable ,input_shape, pool_size=(2, 2, 2), filter_size
     conv1 = BatchNormalization()(conv1)
     conv1 = Conv3D(32, filter_size, padding='same', activation='relu')(conv1)
     conv1 = BatchNormalization()(conv1)
-    # pool1 = Conv3D(32, filter_size, strides=(2,2,2), padding='same', activation='relu')(conv1)
     pool1 = MaxPooling3D(pool_size=pool_size, strides=2)(conv1)
 
     conv2 = Conv3D(32, filter_size, padding='same', activation='relu')(pool1)
     conv2 = BatchNormalization()(conv2)
     conv2 = Conv3D(64, filter_size, padding='same', activation='relu')(conv2)
     conv2 = BatchNormalization()(conv2)
-    # pool2 = Conv3D(64, filter_size, strides=(2,2,2), padding='same', activation='relu')(conv2)
     pool2 = MaxPooling3D(pool_size=pool_size, strides=2)(conv2)
 
     conv3 = Conv3D(64, filter_size, padding='same', activation='relu')(pool2)
     conv3 = BatchNormalization()(conv3)
     conv3 = Conv3D(128, filter_size, padding='same', activation='relu')(conv3)
     conv3 = BatchNormalization()(conv3)
-    # pool3 = Conv3D(128, filter_size, strides=(2,2,2), padding='same', activation='relu')(conv3)
     pool3 = MaxPooling3D(pool_size=pool_size, strides=2)(conv3)
 
     conv4 = Conv3D(128, filter_size, padding='same', activation='relu')(pool3)
@@ -50,7 +48,9 @@ def get_model(logger, log_disable ,input_shape, pool_size=(2, 2, 2), filter_size
     conv4 = Conv3D(256, filter_size, padding='same', activation='relu')(conv4)
     conv4 = BatchNormalization()(conv4)
 
-    up5 = Deconv3D(64,filter_size, strides=(2,2,2), padding='same',activation='relu')(conv4)
+
+    up5 = UpSampling3D(size=pool_size)(conv4)
+    up5 = Conv3D(128, (2, 2, 2), padding='same', activation='relu')(up5)
     up5 = BatchNormalization()(up5)
     up5 = concatenate([up5, conv3], axis=4)
     conv5 = Conv3D(128, filter_size, padding='same', activation='relu')(up5)
@@ -58,7 +58,9 @@ def get_model(logger, log_disable ,input_shape, pool_size=(2, 2, 2), filter_size
     conv5 = Conv3D(128, filter_size, padding='same', activation='relu')(conv5)
     conv5 = BatchNormalization()(conv5)
 
-    up6 = Deconv3D(32,filter_size, strides=(2,2,2), padding='same',activation='relu')(conv5)
+
+    up6 = UpSampling3D(size=pool_size)(conv5)
+    up6 = Conv3D(64, (2, 2, 2), padding='same', activation='relu')(up6)
     up6 = BatchNormalization()(up6)
     up6 = concatenate([up6, conv2], axis=4)
     conv6 = Conv3D(64, filter_size, padding='same', activation='relu')(up6)
@@ -66,7 +68,8 @@ def get_model(logger, log_disable ,input_shape, pool_size=(2, 2, 2), filter_size
     conv6 = Conv3D(64, filter_size, padding='same', activation='relu')(conv6)
     conv6 = BatchNormalization()(conv6)
 
-    up7 = Deconv3D(16, filter_size, strides=(2,2,2), padding='same', activation='relu')(conv6)
+    up7 = UpSampling3D(size=pool_size)(conv6)
+    up7 = Conv3D(32, (2, 2, 2), padding='same', activation='relu')(up7)
     up7 = BatchNormalization()(up7)
     up7 = concatenate([up7, conv1], axis=4)
     conv7 = Conv3D(32, filter_size, padding='same', activation='relu')(up7)
