5eeb0e9d173e7658c3f2e8f5b77dcb78df16a919
HamedShapan <hamedcan@gmail.com>
2017-11-09 15:39:31 +0330

    fix some bugs

--- Model_Vnet.py
+++ Model_Vnet.py
@@ -52,7 +52,7 @@ def get_model(logger, log_disable ,input_shape, pool_size=(2, 2, 2), filter_size
     conv4 = Conv3D(256, filter_size, padding='same', activation='relu')(conv4)
     conv4 = BatchNormalization()(conv4)
 
-    up5 = Deconv3D(128,filter_size,stride=(2,2,2),padding='same',activation='relu')(conv4)
+    up5 = Deconv3D(128,filter_size,strides=(2,2,2),padding='same',activation='relu')(conv4)
     up5 = BatchNormalization()(up5)
     up5 = concatenate([up5, conv3], axis=4)
     conv5 = Conv3D(128, filter_size, padding='same', activation='relu')(up5)
@@ -60,7 +60,7 @@ def get_model(logger, log_disable ,input_shape, pool_size=(2, 2, 2), filter_size
     conv5 = Conv3D(128, filter_size, padding='same', activation='relu')(conv5)
     conv5 = BatchNormalization()(conv5)
 
-    up6 = Deconv3D(64,filter_size,stride=(2,2,2),padding='same',activation='relu')(conv5)
+    up6 = Deconv3D(64,filter_size,strides=(2,2,2),padding='same',activation='relu')(conv5)
     up6 = BatchNormalization()(up6)
     up6 = concatenate([up6, conv2], axis=4)
     conv6 = Conv3D(64, filter_size, padding='same', activation='relu')(up6)
@@ -68,7 +68,7 @@ def get_model(logger, log_disable ,input_shape, pool_size=(2, 2, 2), filter_size
     conv6 = Conv3D(64, filter_size, padding='same', activation='relu')(conv6)
     conv6 = BatchNormalization()(conv6)
 
-    up7 = Deconv3D(32, filter_size, stride=(2, 2, 2), padding='same', activation='relu')(conv6)
+    up7 = Deconv3D(32, filter_size, strides=(2, 2, 2), padding='same', activation='relu')(conv6)
     up7 = BatchNormalization()(up7)
     up7 = concatenate([up7, conv1], axis=4)
     conv7 = Conv3D(32, filter_size, padding='same', activation='relu')(up7)
