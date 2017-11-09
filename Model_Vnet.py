4ca4b933392ce5e61a195ea08ce7b553a5c690cb
hamedcan <32765912+hamedcan@users.noreply.github.com>
2017-11-09 15:48:03 +0330

    deconvolution and conv added

--- Model_Vnet.py
+++ Model_Vnet.py
@@ -31,28 +31,28 @@ def get_model(logger, log_disable ,input_shape, pool_size=(2, 2, 2), filter_size
     conv1 = Conv3D(32, filter_size, padding='same', activation='relu')(conv1)
     conv1 = BatchNormalization()(conv1)
     conv1 = Dropout(drop_rate)(conv1)
-    pool1 = Conv3D(32, filter_size,stride=(2,2,2), padding='same', activation='relu')(conv1)
+    pool1 = Conv3D(32, filter_size, strides=(2,2,2), padding='same', activation='relu')(conv1)
 
     conv2 = Conv3D(32, filter_size, padding='same', activation='relu')(pool1)
     conv2 = BatchNormalization()(conv2)
     conv2 = Conv3D(64, filter_size, padding='same', activation='relu')(conv2)
     conv2 = BatchNormalization()(conv2)
     conv2 = Dropout(drop_rate)(conv2)
-    pool2 = Conv3D(64, filter_size,stride=(2,2,2), padding='same', activation='relu')(conv2)
+    pool2 = Conv3D(64, filter_size, strides=(2,2,2), padding='same', activation='relu')(conv2)
 
     conv3 = Conv3D(64, filter_size, padding='same', activation='relu')(pool2)
     conv3 = BatchNormalization()(conv3)
     conv3 = Conv3D(128, filter_size, padding='same', activation='relu')(conv3)
     conv3 = BatchNormalization()(conv3)
     conv3 = Dropout(drop_rate)(conv3)
-    pool3 = Conv3D(128, filter_size,stride=(2,2,2), padding='same', activation='relu')(conv3)
+    pool3 = Conv3D(128, filter_size, strides=(2,2,2), padding='same', activation='relu')(conv3)
 
     conv4 = Conv3D(128, filter_size, padding='same', activation='relu')(pool3)
     conv4 = BatchNormalization()(conv4)
     conv4 = Conv3D(256, filter_size, padding='same', activation='relu')(conv4)
     conv4 = BatchNormalization()(conv4)
 
-    up5 = Deconv3D(128,filter_size,strides=(2,2,2),padding='same',activation='relu')(conv4)
+    up5 = Deconv3D(128,filter_size, strides=(2,2,2), padding='same',activation='relu')(conv4)
     up5 = BatchNormalization()(up5)
     up5 = concatenate([up5, conv3], axis=4)
     conv5 = Conv3D(128, filter_size, padding='same', activation='relu')(up5)
@@ -60,7 +60,7 @@ def get_model(logger, log_disable ,input_shape, pool_size=(2, 2, 2), filter_size
     conv5 = Conv3D(128, filter_size, padding='same', activation='relu')(conv5)
     conv5 = BatchNormalization()(conv5)
 
-    up6 = Deconv3D(64,filter_size,strides=(2,2,2),padding='same',activation='relu')(conv5)
+    up6 = Deconv3D(64,filter_size, strides=(2,2,2), padding='same',activation='relu')(conv5)
     up6 = BatchNormalization()(up6)
     up6 = concatenate([up6, conv2], axis=4)
     conv6 = Conv3D(64, filter_size, padding='same', activation='relu')(up6)
@@ -68,7 +68,7 @@ def get_model(logger, log_disable ,input_shape, pool_size=(2, 2, 2), filter_size
     conv6 = Conv3D(64, filter_size, padding='same', activation='relu')(conv6)
     conv6 = BatchNormalization()(conv6)
 
-    up7 = Deconv3D(32, filter_size, strides=(2, 2, 2), padding='same', activation='relu')(conv6)
+    up7 = Deconv3D(32, filter_size, strides=(2,2,2), padding='same', activation='relu')(conv6)
     up7 = BatchNormalization()(up7)
     up7 = concatenate([up7, conv1], axis=4)
     conv7 = Conv3D(32, filter_size, padding='same', activation='relu')(up7)
@@ -113,18 +113,3 @@ def compute_level_output_shape(filters, depth, pool_size, image_shape):
     else:
         output_image_shape = image_shape
     return tuple([None, filters] + [int(x) for x in output_image_shape])
-
-
-def get_upconv(depth, nb_filters, pool_size, image_shape, kernel_size=(2, 2, 2), strides=(2, 2, 2),
-               deconvolution=False):
-    if deconvolution:
-        return Deconvolution3D(filters=nb_filters, kernel_size=kernel_size,
-                               output_shape=compute_level_output_shape(filters=nb_filters, depth=depth,
-                                                                       pool_size=pool_size,
-                                                                       image_shape=image_shape),
-                               strides=strides, input_shape=compute_level_output_shape(filters=nb_filters,
-                                                                                       depth=depth + 1,
-                                                                                       pool_size=pool_size,
-                                                                                       image_shape=image_shape))
-    else:
-        return UpSampling3D(size=pool_size)
