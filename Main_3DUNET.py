1235db01c0c1c2b9254387e9abeed50b3b15ee9d
hamedcan <32765912+hamedcan@users.noreply.github.com>
2018-07-10 23:18:15 +0430

    LAST COMMIT

--- Main_3DUNET.py
+++ Main_3DUNET.py
@@ -11,12 +11,12 @@ import scipy.io
 # initialization and prepare data set#########################################################
 patch_size = [40, 40, 16]
 batch_size = 16
-epochs = 200
-repeat = 5
+epochs = 1
+repeat = 1
 channel = 2
 K = 5
 angles = []
-scales = [0.75,0.5,0.25]
+scales = []
 g_path = r'C:\result\\' + datetime.now().strftime('%Y-%m-%d--%H-%M')
 
 ds = DS('.\data\\', patch_size, channel, K, angles, scales)
@@ -36,7 +36,7 @@ logger.flush()
 
 ####################################################################################################
 for fold in range(0, K):
-    x_train, y_train, x_test, y_test, x_train3, y_train3, x_test3, y_test3, x_train4, y_train4, x_test4, y_test4 = ds.get_data(fold)
+    x_train, y_train, x_test, y_test = ds.get_data(fold)
     logger.write('===================fold: ' + str(fold) + '===================\n')
     logger.write('contain:' + str(ds.train_indexes[fold]) + '\n\n')
     print('===================fold: ' + str(fold) + '===================\n')
@@ -49,42 +49,33 @@ for fold in range(0, K):
         path = g_path + r'\fold-' + str(fold) + r'-rep-' + str(repeat_count)
         # train model##################################################################################
         model.fit(x_train, y_train, batch_size=batch_size, epochs=epochs, shuffle=True,
-                  validation_data=(x_test, y_test), verbose=2)
+                  validation_data=(x_test[0], y_test[0]), verbose=2)
         model.save_weights(path + r'\model.hd5')
         # get accuracy on test data####################################################################
-        pred = model.predict(x_test)
-        ds.post_process2(fold, logger, y_test, pred, '1X')
-
-        pred = model.predict(x_test3)
-        ds.post_process2(fold, logger, y_test3, pred, '3X')
-
-        pred = model.predict(x_test4)
-        ds.post_process2(fold, logger, y_test4, pred, '4X')
-
-# logging#####################################################################################
-        logger.write('==========================================\n')
+        for i in range(0, len(x_test)):
+            ds.post_process2(fold, logger, y_test[i], model.predict(x_test[i]), str(i)+'X')
         # save images to file##################################################################
         logger.flush()
-        #
-        # for i in range(0, x_test.shape[0]):
+
+        # for i in range(0, x_test2.shape[0]):
         #     for j in range(0, patch_size[2]):
         #
         #         image = np.zeros([patch_size[0], patch_size[1], 3])
-        #         image[:, :, 0] = x_test[i, :, :, j, 0] + (pred[i, :, :, j, 0]/4)  # red for predicted by model
-        #         image[:, :, 1] = x_test[i, :, :, j, 0]
-        #         image[:, :, 2] = x_test[i, :, :, j, 0]
+        #         image[:, :, 0] = x_test2[i, :, :, j, 0] + (pred[i, :, :, j, 0]/4)  # red for predicted by model
+        #         image[:, :, 1] = x_test2[i, :, :, j, 0]
+        #         image[:, :, 2] = x_test2[i, :, :, j, 0]
         #         plt.imsave(path + r'\test' + '\im-' + str(i) + '-' + str(j) + '-pred.png', image)
         #
         #         image = np.zeros([patch_size[0], patch_size[1], 3])
-        #         image[:, :, 0] = x_test[i, :, :, j, 0]
-        #         image[:, :, 1] = x_test[i, :, :, j, 0] + (y_test[i, :, :, j, 0]/4)  # green for ground truth
-        #         image[:, :, 2] = x_test[i, :, :, j, 0]
+        #         image[:, :, 0] = x_test2[i, :, :, j, 0]
+        #         image[:, :, 1] = x_test2[i, :, :, j, 0] + (y_test2[i, :, :, j, 0]/4)  # green for ground truth
+        #         image[:, :, 2] = x_test2[i, :, :, j, 0]
         #         plt.imsave(path + r'\test' + '\im-' + str(i) + '-' + str(j) + '-gt.png', image)
         #
         #         image = np.zeros([patch_size[0], patch_size[1], 3])
-        #         image[:, :, 0] = x_test[i, :, :, j, 0]
-        #         image[:, :, 1] = x_test[i, :, :, j, 0]
-        #         image[:, :, 2] = x_test[i, :, :, j, 0]
+        #         image[:, :, 0] = x_test2[i, :, :, j, 0]
+        #         image[:, :, 1] = x_test2[i, :, :, j, 0]
+        #         image[:, :, 2] = x_test2[i, :, :, j, 0]
         #         plt.imsave(path + r'\test' + '\im-' + str(i) + '-' + str(j) + '-orig.png', image)
 
 logger.close()
